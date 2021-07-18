cb38b96de6c0c035c59899ff16e493dda00dda6b
henry <1151071+HenrYxZ@users.noreply.github.com>
2021-07-18 21:37:02 +0200

    standarize size to 320, change ground colors

--- create_tex_script.py
+++ create_tex_script.py
@@ -1,16 +1,27 @@
 # Local modules
 import numpy as np
+import json
 
-from utils import create_texture_from_noise
+import utils
+
+CONFIG_FILENAME = "config.json"
+CITIES = ["Jerusalem", "Shechem"]
+SIZE = 320
 
 
 def main():
-    city = "jerusalem"
-    color = np.array([235, 230, 183])
-    dark_color = 0.8 * color
-    size = 640
+    option = int(input(utils.menu_str(CITIES))) - 1
+    city = CITIES[option].lower()
+    config_path = f"assets/{city}/{CONFIG_FILENAME}"
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    color = np.array(config['groundColor'])
+    if config.get('darkColor'):
+        dark_color = np.array(config['darkColor'])
+    else:
+        dark_color = 0.8 * color
     noise_path = "assets/perlin_noise.png"
-    img = create_texture_from_noise(color, size, noise_path, dark_color)
+    img = utils.create_texture_from_noise(color, SIZE, noise_path, dark_color)
     img.save(f'assets/{city}/ground.png')
 
 
