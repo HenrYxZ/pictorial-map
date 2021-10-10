270b850098938232c1f8db2e4a7934a895b990a4
henry <1151071+HenrYxZ@users.noreply.github.com>
2021-10-09 22:23:09 -0300

    refactor: add maps list to main menu programmatically with maps.json

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+const mapsElement = document.getElementById("maps-list");
+const mapsResponse = await fetch('../js/maps.json');
+const maps = await mapsResponse.json();
+for (const map of maps) {
+    let mapElement = document.createElement("a");
+    mapElement.classList.add("list-group-item");
+    mapElement.classList.add("list-group-item-action");
+    mapElement.innerHTML = map;
+    mapElement.href = map;
+    mapsElement.appendChild(mapElement);
+}
